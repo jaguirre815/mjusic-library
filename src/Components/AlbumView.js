e5df53ebd27a7f933e9b975b3f08b252d0f53eb2
Joel Aguirre <joelaguirre@Joels-MacBook-Pro.local>
2022-09-14 20:57:20 -0500

    added navbuttons

--- src/Components/AlbumView.js
+++ src/Components/AlbumView.js
@@ -1,16 +1,44 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import {useNavigate } from 'react-router-dom'
 
 function AlbumView() {
+    const navigate = useNavigate()
     const { id } = useParams()
-    const [ albumData, setalbumData ] = useState([])
+    const [ albumData, setAlbumData ] = useState([])
+
+    const navButtons = () => {
+        return(
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justSongs = albumData.filter(entry => entry.wrapperType === 'track')
+
+    const renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key = >
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
 
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+            {renderSongs}
         </div>
     )
 }
-
-export default AlbumView
